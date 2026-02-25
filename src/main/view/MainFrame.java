7cbceaab38893182050b88d04cb56701f59ac4b4
Mohamed hassan <mohamed_edhassan@yahoo.com>
2026-02-25 16:21:12 -0600

    completed the MainFrame.java file

--- src/main/view/MainFrame.java
+++ src/main/view/MainFrame.java
@@ -1,12 +1,26 @@
+/*
+MainFrame.java class
+
+This class represents the main frame of the simulation.
+
+@author: Mohamed Hassan
+@date: 2026-02-25
+*/
 package main.view;
 
+//importing the required libraries
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 import java.awt.BorderLayout;
 
 public class MainFrame extends JFrame {
+    //instance variables
+    private ControlPanel controlPanel;
+    private RingPanel ringPanel;
 
+    //constructor
     public MainFrame(){
+        //setting the title, size, and default close operation of the frame
         setTitle("Consistent Hashing Simulation");
         setSize(1200, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -14,8 +28,21 @@ public class MainFrame extends JFrame {
 
         ControlPanel controlPanel = new ControlPanel();
         RingPanel ringPanel = new RingPanel();
-        JPanel statusPanel = new JPanel();
-        JPanel logPanel = new JPanel();
+        //creating the main split pane
+        JSplitPane mainSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT , controlPanel, ringPanel);
+        //setting the divider location to 250
+        mainSplitPane.setDividerLocation(250);
+        //adding the main split pane to the frame
+        add(mainSplitPane,BorderLayout.CENTER);
+        setVisible(true);
+    }
+
+    //getters
+    public ControlPanel getControlPanel() {
+        return controlPanel;
+    }
+    public RingPanel getRingPanel() {
+        return ringPanel;
     }
 
     
